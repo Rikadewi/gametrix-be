c66f3d561e219149719fa7cfa31a2d616f3b214f
Rika Dewi <rika_4444@yahoo.com>
2020-03-07 23:35:54 +0700

    Add websocket server

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-})
+var expressWs = require('express-ws')(app);
+var port = 3000;
 
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
 
-    console.log("Example app listening at http://%s:%s", host, port)
-})
+app.use(function (req, res, next) {
+    console.log('middleware');
+    req.testing = 'testing';
+    return next();
+});
+
+app.get('/', function (req, res, next) {
+    console.log('get route', req.testing);
+    res.end();
+});
+
+app.ws('/', function (ws, req) {
+    ws.on('message', function (msg) {
+        console.log("on message");
+        console.log(msg);
+    });
+    console.log('socket', req.testing);
+
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+app.listen(port, function () {
+    console.log("App listening at ws://localhost:%s", port);
+});
